20b07c0d998f3f28fef08f914b7367f83a1760f7
feliperodsdev <libertyisrequired@gmail.com>
2023-04-05 08:49:27 -0300

    feat: UpdatePizzaDto

---
+++ pizza-service/src/main/java/com/feliperodsdev/ms/pizzaservice/dtos/UpdatePizzaDto.java
@@ -0,0 +1,32 @@
+package com.feliperodsdev.ms.pizzaservice.dtos;
+
+public class UpdatePizzaDto {
+
+    private String name;
+    private String desc;
+    private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
