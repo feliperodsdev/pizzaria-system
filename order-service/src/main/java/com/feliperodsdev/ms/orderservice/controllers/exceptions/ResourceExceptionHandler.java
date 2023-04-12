cbadf1cd453f9d5f0e2130bafbf9f5170abe0781
feliperodsdev <libertyisrequired@gmail.com>
2023-04-12 13:32:49 -0300

    feat: resource not found exception

--- order-service/src/main/java/com/feliperodsdev/ms/orderservice/controllers/exceptions/ResourceExceptionHandler.java
+++ order-service/src/main/java/com/feliperodsdev/ms/orderservice/controllers/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package com.feliperodsdev.ms.orderservice.controllers.exceptions;
 import com.feliperodsdev.ms.orderservice.dtos.HttpResponseDto;
 import com.feliperodsdev.ms.orderservice.model.exceptions.EntityValidationException;
 import com.feliperodsdev.ms.orderservice.services.exceptions.ResourceAlreadyExists;
+import com.feliperodsdev.ms.orderservice.services.exceptions.ResourceNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,6 +26,15 @@ public class ResourceExceptionHandler {
         HttpResponseDto response = new HttpResponseDto();
         String error = "Error";
         HttpStatus statusCode = HttpStatus.BAD_REQUEST;
+        StandardError standardError = new StandardError(statusCode, error, e.getMessage());
+        return response.badRequest(standardError);
+    }
+
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<Object> notFound(ResourceNotFound e){
+        HttpResponseDto response = new HttpResponseDto();
+        String error = "Error";
+        HttpStatus statusCode = HttpStatus.NOT_FOUND;
         StandardError standardError = new StandardError(statusCode, error, e.getMessage());
         return response.badRequest(standardError);
     }
---
+++ order-service/src/main/java/com/feliperodsdev/ms/orderservice/services/exceptions/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.feliperodsdev.ms.orderservice.services.exceptions;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String msg){
+        super(msg);
+    }
+}
